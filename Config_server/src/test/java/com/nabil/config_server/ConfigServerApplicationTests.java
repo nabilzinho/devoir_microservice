3d8b649369b9f6fd8b12d949f24b215502503575
nabil <nabildorhmi2001@gmail.com>
2023-12-22 12:29:08 +0100

    commit 2

---
+++ Config_server/src/test/java/com/nabil/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nabil.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
